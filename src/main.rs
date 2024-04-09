b1e090a20ce7af89aaa818de55e6b2c5a9b5cfcf
Aditya Goyal <agoyal7000@gmail.com>
2024-04-09 07:02:03 -0600

    Adds difficulty select

--- src/main.rs
+++ src/main.rs
@@ -4,20 +4,26 @@ use menu_bar::*;
 
 #[function_component(App)]
 fn app() -> Html {
+    // ---------------Menu Bar----------------
     let games = vec![
         Game {
-            name: "Connect-4".to_string()
-        }, 
+            name: "Connect-4".to_string(),
+        },
         Game {
-            name: "OTTO".to_string()
-        }
+            name: "OTTO".to_string(),
+        },
     ];
-
-    let selected_game = use_state(||games[0].clone()); //Default game
-
+    let selected_game = use_state(|| games[0].clone()); //Default game
     let on_game_select = {
         let selected_game = selected_game.clone();
         Callback::from(move |game: Game| selected_game.set(game))
+    };
+
+    let difficulties = vec![Difficulty::Easy, Difficulty::Hard];
+    let selected_difficulty = use_state(|| difficulties[0].clone()); //Default difficulty
+    let on_difficulty_select = {
+        let selected_difficulty = selected_difficulty.clone();
+        Callback::from(move |difficulty: Difficulty| selected_difficulty.set(difficulty))
     };
 
     html! {
@@ -26,6 +32,11 @@ fn app() -> Html {
         <GamesList games={games}
         selected_game = {(*selected_game).clone()}
         on_click={on_game_select.clone()} />
+        </div>
+        <div>
+        <DifficultyList difficulties={difficulties}
+        selected_difficulty = {(*selected_difficulty).clone()}
+        on_click={on_difficulty_select.clone()} />
         </div>
     </>
     }
--- src/menu_bar.rs
+++ src/menu_bar.rs
@@ -4,21 +4,33 @@ use yew::prelude::*;
 pub struct Game {
     pub name: String,
 }
+#[derive(Clone, PartialEq, Debug)]
+pub enum Difficulty {
+    Easy,
+    Hard,
+}
 
 #[derive(Properties, PartialEq)]
-pub struct State {
+pub struct MenuGameSelect {
     pub games: Vec<Game>,
     pub selected_game: Game,
     pub on_click: Callback<Game>,
 }
 
+#[derive(Properties, PartialEq)]
+pub struct MenuDifficultySelect {
+    pub difficulties: Vec<Difficulty>,
+    pub selected_difficulty: Difficulty,
+    pub on_click: Callback<Difficulty>,
+}
+
 #[function_component(GamesList)]
 pub fn games_list(
-    State {
+    MenuGameSelect {
         games,
         selected_game,
         on_click,
-    }: &State,
+    }: &MenuGameSelect,
 ) -> Html {
     let on_click = on_click.clone();
     let game_options_html = games
@@ -42,4 +54,36 @@ pub fn games_list(
         .collect();
 
     game_options_html
+}
+
+#[function_component(DifficultyList)]
+pub fn difficulty_list(
+    MenuDifficultySelect {
+        difficulties,
+        selected_difficulty,
+        on_click,
+    }: &MenuDifficultySelect,
+) -> Html {
+    let on_click = on_click.clone();
+    let difficulty_options_html = difficulties
+        .iter()
+        .map(|difficulty| {
+            let on_difficulty_select = {
+                let on_click = on_click.clone();
+                let difficulty = difficulty.clone();
+                Callback::from(move |_| on_click.emit(difficulty.clone()))
+            };
+            if difficulty == selected_difficulty {
+                html! {
+                    <b onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</b>
+                }
+            } else {
+                html! {
+                    <p onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</p>
+                }
+            }   
+        })
+        .collect();
+
+    difficulty_options_html
 }
