9992a1d00ed37e747466f9edfb3975506eb8e629
Aditya Goyal <agoyal7000@gmail.com>
2024-04-10 01:24:26 -0600

    Formats menu and adds rules button

--- src/main.rs
+++ src/main.rs
@@ -8,9 +8,14 @@ fn app() -> Html {
     let games = vec![
         Game {
             name: "Connect-4".to_string(),
+            rules: "Players take turns inserting tokens.
+                    Tokens can only be inserted to the top of the column.
+                    Connect 4 of your color in a straight row  to win.
+                    Rows can be horizontal, vertical, or diagonal.".to_string()
         },
         Game {
             name: "OTTO".to_string(),
+            rules: "TODO".to_string()
         },
     ];
     let selected_game = use_state(|| games[0].clone()); //Default game
@@ -26,18 +31,32 @@ fn app() -> Html {
         Callback::from(move |difficulty: Difficulty| selected_difficulty.set(difficulty))
     };
 
+    let rules_opened = use_state(|| false);
+    let on_rules_select = {
+        let rules_opened = rules_opened.clone();
+        Callback::from(move |opened: bool| rules_opened.set(opened))
+    };
+
     html! {
     <>
-        <div>
-        <GamesList games={games}
-        selected_game = {(*selected_game).clone()}
-        on_click={on_game_select.clone()} />
+        <div style="padding:10px; text-align: center;">
+            <div style="display:inline; margin:20px">
+                <GamesList games={games}
+                selected_game = {(*selected_game).clone()}
+                on_click={on_game_select.clone()}/>
+            </div>
+            <div style="display:inline; margin:20px">
+                <DifficultyList difficulties={difficulties}
+                selected_difficulty = {(*selected_difficulty).clone()}
+                on_click={on_difficulty_select.clone()} />
+            </div>
+            <div style="padding:10px;"> 
+                <RulesButton selected_game = {(*selected_game).clone()}
+                rules_opened={(*rules_opened).clone()}
+                on_click={on_rules_select.clone()}/>
+            </div>
         </div>
-        <div>
-        <DifficultyList difficulties={difficulties}
-        selected_difficulty = {(*selected_difficulty).clone()}
-        on_click={on_difficulty_select.clone()} />
-        </div>
+        
     </>
     }
 }
--- src/menu_bar.rs
+++ src/menu_bar.rs
@@ -3,6 +3,7 @@ use yew::prelude::*;
 #[derive(Clone, PartialEq)]
 pub struct Game {
     pub name: String,
+    pub rules: String,
 }
 #[derive(Clone, PartialEq, Debug)]
 pub enum Difficulty {
@@ -24,6 +25,13 @@ pub struct MenuDifficultySelect {
     pub on_click: Callback<Difficulty>,
 }
 
+#[derive(Properties, PartialEq)]
+pub struct MenuRulesButton {
+    pub selected_game: Game,
+    pub on_click: Callback<bool>,
+    pub rules_opened: bool,
+}
+
 #[function_component(GamesList)]
 pub fn games_list(
     MenuGameSelect {
@@ -43,11 +51,11 @@ pub fn games_list(
             };
             if game == selected_game {
                 html! {
-                    <b onclick={on_game_select} id="game_option">{format!("{}", game.name)}</b>
+                    <u style="padding: 10px; display: inline;" onclick={on_game_select} id="game_option"> {format!("{}", game.name)}</u>
                 }
             } else {
                 html! {
-                    <p onclick={on_game_select} id="game_option">{format!("{}", game.name)}</p>
+                    <p style="padding: 10px; display: inline;" onclick={on_game_select} id="game_option">{format!("{}", game.name)}</p>
                 }
             }   
         })
@@ -75,15 +83,45 @@ pub fn difficulty_list(
             };
             if difficulty == selected_difficulty {
                 html! {
-                    <b onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</b>
+                    <u style="padding: 10px; display: inline;" onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</u>
                 }
             } else {
                 html! {
-                    <p onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</p>
+                    <p style="padding: 10px; display: inline;" onclick={on_difficulty_select} id="difficulty_option">{format!("{:?}", difficulty)}</p>
                 }
             }   
         })
         .collect();
-
     difficulty_options_html
+    
+}
+
+#[function_component(RulesButton)]
+pub fn rules_button(
+    MenuRulesButton {
+        selected_game,
+        on_click,
+        rules_opened,
+    }: &MenuRulesButton,
+) -> Html {
+    let on_click = on_click.clone();
+
+    if *rules_opened {
+        let on_rules_select = Callback::from(move |_| on_click.emit(false));
+        html! {
+            <>
+                <u style="padding: 10px; display: inline;" 
+                onclick={on_rules_select} id="difficulty_option">{"Rules!"}</u>
+                <p style="padding: 2px; width: 50ch; text-align: center; margin: 0 auto;">
+                {selected_game.rules.clone()}
+                </p>
+            </>
+        }
+    } else {
+        let on_rules_select = Callback::from(move |_| on_click.emit(true));
+        html! {
+            <p style="padding: 10px; display: inline;" 
+            onclick={on_rules_select} id="difficulty_option">{"Rules!"}</p>
+        }
+    }
 }
