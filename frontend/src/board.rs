83e1a7a25d76164b3bb56266cb67723ec73e15c0
Rahul <rtabraha@ualberta.ca>
2024-04-11 23:27:17 -0600

    create cell component

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -7,14 +7,20 @@ pub struct ColProps {
     col_index: usize,
     #[prop_or(Color::Empty)]
     color: Color,
-    pub on_click: Callback<usize>,
+    pub on_click: Callback<Cell>,
+}
+#[derive(Properties, Clone, PartialEq)]
+pub struct CellProps {
+    color: Color,
+    x: usize,
+    y: usize,
 }
 
 #[derive(Clone, PartialEq)]
 pub enum Color {
+    Empty,
     Red,
     Yellow,
-    Empty,
 }
 
 pub enum Msg {
@@ -23,11 +29,11 @@ pub enum Msg {
 
 #[function_component(Board)]
 pub fn board(props: &BoardProps) -> Html {
-    let selected_column = use_state(|| None);
-    let on_column_select = {
-        let selected_column = selected_column.clone();
-        Callback::from(move |col_index| {
-            selected_column.set(Some(col_index));
+    let on_click = {
+        let on_click = use_state(|| Callback::noop());
+        let on_click = on_click.clone();
+        Callback::from(move |cell: Cell| {
+            on_click.emit(cell);
         })
     };
 
@@ -35,7 +41,7 @@ pub fn board(props: &BoardProps) -> Html {
         <board>
             {for (0..7).map(|col_index| {
                 html! {
-                    <Column col_index={col_index} on_click={on_column_select.clone()} />
+                    <Column col_index={col_index} on_click={on_click.clone()} />
                 }
             })}
         </board>
@@ -47,16 +53,23 @@ pub fn column(props: &ColProps) -> Html {
     html! {
         <column>
             {for (0..6).map(|row_index| {
-                let cell_id = format!("c{},{}", row_index, props.col_index);
-                let cell_color = match props.color {
-                    Color::Red => "red",
-                    Color::Yellow => "yellow",
-                    Color::Empty => "white",
-                };
                 html! {
-                    <cell id={cell_id} style={format!("background-color: {}", cell_color)}></cell>
+                    <Cell color={props.color.clone()} x={row_index} y={props.col_index} />
                 }
             })}
         </column>
+    }
+}
+
+#[function_component(Cell)]
+pub fn cell(props: &CellProps) -> Html {
+    let cell_id = format!("c{},{}", props.x, props.y);
+    let cell_color = match props.color {
+        Color::Red => "red",
+        Color::Yellow => "yellow",
+        Color::Empty => "white",
+    };
+    html! {
+        <cell id={cell_id} style={format!("background-color:{}", cell_color)}></cell>
     }
 }
