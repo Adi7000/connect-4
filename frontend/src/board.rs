741e02f71b16d4e7be1b7896a1e99678a65b4ca1
Aditya Goyal <agoyal7000@gmail.com>
2024-04-12 19:24:40 -0600

    Adds connect 4 win checking

--- frontend/src/board.rs
+++ frontend/src/board.rs
@@ -15,7 +15,7 @@ pub struct CellProps {
     y: usize,
 }
 
-#[derive(Clone, PartialEq, Copy)]
+#[derive(Clone, PartialEq, Copy, Debug)]
 pub enum Color {
     Empty,
     Red,
@@ -25,6 +25,159 @@ pub enum Color {
 pub enum Msg {
     ColumnClicked(usize),
 }
+
+pub fn check_for_win(board_state: Vec<Vec<Color>>) -> Option<Color> {
+     
+    let mut consecutive_count: i32;
+    let mut current_color: Color;
+
+    //check vertical
+    for column in &board_state {
+        //handle first item in column
+        consecutive_count = 1;
+        current_color = column[0];
+        for cell in &column[1..] {
+            if *cell != Color::Empty {
+                if current_color == *cell { //handle next item is same color
+                    consecutive_count += 1;
+                } else { //handle next item is different color
+                    consecutive_count = 1;
+                    current_color = *cell
+                }
+                //check 4 consecutives
+                if consecutive_count==4 {
+                    return Some(current_color)
+                }
+            } else {
+                //skip if empty
+                consecutive_count = 0;
+                current_color = Color::Empty;
+            }
+            
+        }
+    }
+
+    //check horizontal win
+    for row_id in 0..6 {
+        //handle first item in row
+        consecutive_count = 1;
+        current_color = board_state[0][row_id];
+        for col_id in 1..7 {
+            let cell = board_state[col_id][row_id];
+            if cell != Color::Empty {
+                if current_color == cell { //handle next item is same color
+                    consecutive_count += 1;
+                } else { //handle next item is different color
+                    consecutive_count = 1;
+                    current_color = cell
+                }
+                //check 4 consecutives
+                if consecutive_count==4 {
+                    return Some(current_color)
+                }
+            } else {
+                //skip if empty
+                consecutive_count = 0;
+                current_color = Color::Empty;
+            }
+            
+        }
+    }
+
+    let max_row:i32 = 6;
+    let max_col:i32 = 7;
+
+    //check diagonal win (bottom left to top right)
+    for diagonal_id in 0..(max_row+max_col-1) { //similar to row_id
+        //handle first item in diagonal
+        consecutive_count = 1;
+        current_color = if diagonal_id < max_row {
+            board_state[0][diagonal_id as usize]
+        } else {
+            Color::Empty
+        };
+        let mut col_id=1;
+        let mut row_id=diagonal_id-1;
+        loop {
+            if row_id < 0 || col_id >= max_col {
+                break;
+            };
+            let cell = if row_id >= max_row {
+                Color::Empty
+            } else {
+                board_state[col_id as usize][row_id as usize]
+            };
+            
+
+            if cell != Color::Empty {
+                if current_color == cell { //handle next item is same color
+                    consecutive_count += 1;
+                } else { //handle next item is different color
+                    consecutive_count = 1;
+                    current_color = cell;
+                }
+                //check 4 consecutives
+                if consecutive_count==4 {
+                    return Some(current_color);
+                }
+            } else {
+                //skip if empty
+                consecutive_count = 0;
+                current_color = Color::Empty;
+            }
+            col_id+=1;
+            row_id-=1;
+        }
+    };
+
+    //check diagonal win 2 (top left to bottom right)
+    for diagonal_id in 0..(max_row+max_col-1) { //similar to row_id
+        //handle first item in diagonal
+        consecutive_count = 1;
+        current_color = if diagonal_id < max_row {
+            board_state[(max_col-1) as usize][diagonal_id as usize]
+        } else {
+            Color::Empty
+        };
+        let mut col_id=max_col - 2;
+        let mut row_id=diagonal_id-1;
+        loop {
+            if row_id < 0 || col_id < 0 {
+                break;
+            };
+            let cell = if row_id >= max_row {
+                Color::Empty
+            } else {
+                board_state[col_id as usize][row_id as usize]
+            };
+            
+
+            if cell != Color::Empty {
+                if current_color == cell { //handle next item is same color
+                    consecutive_count += 1;
+                } else { //handle next item is different color
+                    consecutive_count = 1;
+                    current_color = cell;
+                }
+                //check 4 consecutives
+                if consecutive_count==4 {
+                    return Some(current_color);
+                }
+            } else {
+                //skip if empty
+                consecutive_count = 0;
+                current_color = Color::Empty;
+            }
+            col_id-=1;
+            row_id-=1;
+        }
+    }
+
+    None
+}
+
+
+
 
 #[function_component(Board)]
 pub fn board(_props: &BoardProps) -> Html {
@@ -42,13 +195,18 @@ pub fn board(_props: &BoardProps) -> Html {
                     break;
                 }
             }
-            set_cell_colors.set(new_cell_colors);
+            set_cell_colors.set(new_cell_colors.clone());
             current_player.set(match *current_player {
                 // Add these lines
                 Color::Red => Color::Yellow,
                 Color::Yellow => Color::Red,
                 _ => unreachable!(),
             });
+            //Check for win 
+            if let Some(winner) = check_for_win(new_cell_colors.clone()) {
+                //set all cell colors to winner
+                set_cell_colors.set(vec![vec![winner; 6]; 7]);
+            }
         })
     };
 
